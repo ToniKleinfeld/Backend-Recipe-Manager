83cfacb571518816313ef474e3b6ac57ddf6129e
Toni Kleinfeld <54000942+gaunerleechy@users.noreply.github.com>
2025-10-16 14:44:31 +0200

    Add Maven Wrapper and initial project setup with Spring Boot configuration

---
+++ src/main/java/com/recipemanager/RecipeManagerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.recipemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeManagerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeManagerBackendApplication.class, args);
+	}
+
+}
